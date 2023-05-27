57b41aff445b6c29e453282b3796f0fe807dd08c
Anton Plauche <antonplauche92@gmail.com>
2023-05-27 08:37:55 -0500

    basic static block

---
+++ basic-static/src/edit.js
@@ -0,0 +1,49 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import { useBlockProps,RichText } from '@wordpress/block-editor';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function Edit({attributes, setAttributes}) {
+
+	const blockProps = useBlockProps({className: "basic-block"})
+
+	return (
+		<div { ...blockProps }>
+			<RichText
+					tagName="h2" // The tag here is the element output and editable in the admin
+					value={ attributes.content } // Any existing content, either from the database or an attribute default
+					allowedFormats={ [ 'core/bold', 'core/italic' ] } // Allow the content to be made bold or italic, but do not allow other formatting options
+					onChange={ ( content ) => setAttributes( { content } ) } // Store updated content as a block attribute
+					placeholder={ __( 'Enter your banner text...' ) } // Display this text before any content has been added by the user
+			/>
+		</div>
+	);
+}
+
+
