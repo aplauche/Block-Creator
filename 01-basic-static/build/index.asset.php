0ec9f7913fe7471021394dd2b22f890f44385a1d
Anton Plauche <antonplauche92@gmail.com>
2023-06-01 21:33:29 -0500

    rm cached builds

--- 01-basic-static/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '1dc5c6996580e6c1464b');
--- 02-block-supports/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '0d509dec60a75b338ded');
