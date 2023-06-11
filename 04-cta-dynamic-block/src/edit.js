303bda6c39a0078044bec313998fe7fa2bfe1928
Anton Plauche <antonplauche92@gmail.com>
2023-06-11 10:55:32 -0500

    dynamic block using url input button complete

--- 04-cta-dynamic-block/src/edit.js
+++ 04-cta-dynamic-block/src/edit.js
@@ -11,7 +11,9 @@ import { __ } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import { useBlockProps, RichText, MediaPlaceholder } from '@wordpress/block-editor';
+import { useBlockProps, RichText, MediaPlaceholder, BlockControls, URLInputButton, InspectorControls } from '@wordpress/block-editor';
+
+import { ToolbarGroup, PanelBody, SelectControl } from '@wordpress/components'
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -29,52 +31,95 @@ import './editor.scss';
  *
  * @return {WPElement} Element to render.
  */
-export default function Edit() {
+export default function Edit({attributes, setAttributes}) {
 	return (
-		<div { ...useBlockProps() }>
-			<div className='cta-image-container'>
-				{attributes.image_url && attributes.image_id ? (
-							<img src={attributes.image_url} />
-							<button onClick={() => setAttributes({image_url: "", image_id: null})}>Remove</button>
-					) : (
-							<MediaPlaceholder
-									onSelect = {
-										( image ) => {
-											setAttributes( { image_url: image.url, image_id: image.id } );
+		<>
+
+			{/* Begin Toolbar Zone */}
+			<BlockControls>
+				<ToolbarGroup>
+					<URLInputButton
+						url={ attributes.button_url }
+						onChange={ ( url, post ) => setAttributes( { button_url: url }) }
+					/>
+				</ToolbarGroup>
+			</BlockControls>
+			{/* End Toolbar Zone */}
+
+			{/* Begin Sidebar Inspector Zone */}
+			<InspectorControls>
+				<PanelBody title="Settings">
+					<SelectControl 
+						label="Layout Variant:"
+						onChange={(val) => setAttributes({layout_variant: val})}
+						value={attributes.layout_variant}
+						options={
+							[
+								{
+									label: "Text Right",
+									value: "text-right"
+								},
+								{
+									label: "Text Left",
+									value: "text-left"
+								}
+							]
+						}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			{/* End Sidebar Inspector Zone */}
+
+			{/* Begin Main Block Zone */}
+			<div { ...useBlockProps({className: `variant-${attributes.layout_variant}`}) }>
+				<div className='cta-image-container'>
+					{attributes.image_url && attributes.image_id ? (
+							<>
+								<img src={attributes.image_url} />
+								<button className="button-remove" onClick={() => setAttributes({image_url: "", image_id: null})}>Remove</button>
+							</>
+						) : (
+								<MediaPlaceholder
+										onSelect = {
+											( image ) => {
+												setAttributes( { image_url: image.url, image_id: image.id } );
+											}
 										}
-									}
-									allowedTypes = { [ 'image' ] }
-									multiple = { false }
-									labels = { { title: 'CTA Image' } }
-								>
-							</MediaPlaceholder>
-					)	
-				}
+										allowedTypes = { [ 'image' ] }
+										multiple = { false }
+										labels = { { title: 'CTA Image' } }
+									>
+								</MediaPlaceholder>
+						)	
+					}
+				</div>
+				<div className='cta-text-container'>
+					<RichText 
+						tagName='h2'
+						allowedFormats={[]}
+						value={attributes.heading}
+						onChange={(heading) => setAttributes({heading})}
+						placeholder='This is the headline'
+					/>
+					<RichText 
+						tagName='p'
+						allowedFormats={[]}
+						value={attributes.body}
+						onChange={(body) => setAttributes({body})}
+						placeholder='This is the body copy'
+					/>
+					<RichText 
+						tagName='div'
+						className='wp-element-button'
+						allowedFormats={[]}
+						value={attributes.button_text}
+						onChange={(button_text) => setAttributes({button_text})}
+						placeholder='Button'
+					/>
+				</div>
 			</div>
-			<div className='cta-text-container'>
-				<RichText 
-					tagName='h2'
-					allowedFormats={[]}
-					value={attributes.heading}
-					onChange={(heading) => setAttributes({heading})}
-					placeholder='This is the headline'
-				/>
-				<RichText 
-					tagName='p'
-					allowedFormats={[]}
-					value={attributes.body}
-					onChange={(body) => setAttributes({body})}
-					placeholder='This is the body copy'
-				/>
-				<RichText 
-					tagName='div'
-					className='wp-element-button'
-					allowedFormats={[]}
-					value={attributes.button_text}
-					onChange={(button_text) => setAttributes({button_text})}
-					placeholder='Button'
-				/>
-			</div>
-		</div>
+			{/* End Main Block Zone */}
+
+		</>
 	);
 }
--- 04-cta-dynamic-block/src/render.php
+++ 04-cta-dynamic-block/src/render.php
@@ -10,21 +10,21 @@
 $heading = $attributes["heading"];
 $body = $attributes["body"];
 $image_id = $attributes["image_id"];
-$url = $attributes["url"];
-$text = $attributes["text"];
+$button_url = $attributes["button_url"];
+$button_text = $attributes["button_text"];
 
 ?>
 
 
-<div <?php echo get_block_wrapper_attributes(); ?>>
-	<div class="dcta-image-container">
+<div <?php echo get_block_wrapper_attributes(["class" => 'variant-' . $attributes["layout_variant"]]); ?>>
+	<div class="cta-image-container">
 		<?php echo wp_get_attachment_image( $image_id, "full" ); ?>
 	</div>
-	<div class="dcta-text-container">
+	<div class="cta-text-container">
 		<h2><?php echo $heading ?></h2>
 		<p><?php echo $body ?></p>
-		<a href="<?php echo $url ?>" class="wp-element-button">
-			<?php echo $text ?>
+		<a href="<?php echo $button_url ?>" class="wp-element-button">
+			<?php echo $button_text ?>
 		</a>
 	</div>
 </div>
