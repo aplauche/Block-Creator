81e692bfcb157c738a30314d873e3460d9e8941d
Anton Plauche <antonplauche92@gmail.com>
2023-10-14 19:51:42 -0500

    add the acf demo block

---
+++ 09-acf-demo-block/acf-cta-block/render.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * ACF Call to action Block template.
+ *
+ * @param array $block The block settings and attributes.
+ * @param string $content Inner blocks content if jsx is enabled
+ * @param boolean $is_preview True during preview render
+ * @param array $context any context variables from parent blocks if applicable
+ */
+
+
+// Load values and assign defaults.
+
+$layout_variant = get_field("layout_variant");
+$image = get_field("image");
+$heading = get_field("heading");
+$body = get_field("body");
+$link = get_field("link");
+
+?>
+
+<div <?php echo get_block_wrapper_attributes(["class" => 'acf-cta-block variant-' . $layout_variant]); ?>>
+	<div class="cta-image-container">
+		<?php echo wp_get_attachment_image( $image, "full" ); ?>
+	</div>
+	<div class="cta-text-container">
+		<h2><?php echo esc_html($heading) ?></h2>
+		<p><?php echo esc_html($body) ?></p>
+    <?php if(!empty($link)): ?>
+		<a target="<?php echo esc_attr($link["target"]) ?>" href="<?php echo esc_attr($link["url"]) ?>" class="wp-element-button">
+			<?php echo esc_html($link["title"]) ?>
+		</a>
+    <?php endif; ?>
+	</div>
+</div>
