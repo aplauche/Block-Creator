526f8924126c5431de949f589e9c607f4eb96794
Anton Plauche-Capers <50935059+aplauche@users.noreply.github.com>
2025-09-19 11:46:23 -0500

    Update index.js

--- 04-cta-dynamic-block/src/index.js
+++ 04-cta-dynamic-block/src/index.js
@@ -13,6 +13,7 @@ import { registerBlockType } from '@wordpress/blocks';
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
 import './style.scss';
+import ‘./editor.scss’;
 
 /**
  * Internal dependencies
